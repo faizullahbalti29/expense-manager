eff0f64cc141da75ee1a41200116923fbdddf7ef
faizullah <faizullahbalti29@gmail.com>
2026-05-28 00:31:36 +0500

    dashboard routing cleared

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -37,19 +37,33 @@ export default function Dashboard() {
   const [authChecked, setAuthChecked] = useState(false);
   const isClient = typeof window !== "undefined";
   const [drawerOpen, setDrawerOpen] = useState(false);
+  useEffect(() => {
+    router.replace("/");
+  }, []);
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Container maxWidth={false} sx={{ mt: 4, mb: 4 }}>
-        <ExpenseStats />
-      </Container>
+    // <Box sx={{ flexGrow: 1 }}>
+    //   <Container maxWidth={false} sx={{ mt: 4, mb: 4 }}>
+    //     <ExpenseStats />
+    //   </Container>
 
-      <Container maxWidth={false} sx={{ mt: 4, mb: 4 }}>
-        <ExpenseForm />
-      </Container>
-      <Container maxWidth={false} sx={{ mt: 2, mb: 4 }}>
-        <ExpenseTable />
-      </Container>
+    //   <Container maxWidth={false} sx={{ mt: 4, mb: 4 }}>
+    //     <ExpenseForm />
+    //   </Container>
+    //   <Container maxWidth={false} sx={{ mt: 2, mb: 4 }}>
+    //     <ExpenseTable />
+    //   </Container>
+    // </Box>
+    <Box
+      sx={{
+        width: "100%",
+        hight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <CircularProgress />
     </Box>
   );
 }
